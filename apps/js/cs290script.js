28647f4a05700702aa585fda7e19aa1dcaff9d98
Kyu Sung Kwon <ryanksk@gmail.com>
2021-09-27 22:57:38 -0700

    update 16

--- apps/js/cs290script.js
+++ apps/js/cs290script.js
@@ -1,17 +1,18 @@
+//from w3 schools: https://www.w3schools.com/howto/howto_js_slideshow.asp
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("myAssignSlides");
-    var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName("cs290_bodycontent_slides");
+    var dots = document.getElementsByClassName("cs290_bodycontent_slide_pos_dot");
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
     slideIndex++;
     if (slideIndex > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
