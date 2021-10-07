5652f33ecbefe8761d2d19dcdd76926037666192
Kyu Sung Kwon <ryanksk@gmail.com>
2021-10-07 14:16:00 -0700

    update 21
    slide change to manual

--- apps/js/cs290script.js
+++ apps/js/cs290script.js
@@ -1,10 +1,11 @@
 //from w3 schools: https://www.w3schools.com/howto/howto_js_slideshow.asp
+/*
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("cs290_bodycontent_slides");
+    var slides = document.getElementsByClassName("cs290_bodycontent_group_slides");
     var dots = document.getElementsByClassName("cs290_bodycontent_slide_pos_dot");
 
     for (i = 0; i < slides.length; i++) {
@@ -21,4 +22,28 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000); //Change every 5 secs
+}
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("cs290_bodycontent_group_slides");
+  var dots = document.getElementsByClassName("cs290_bodycontent_slide_pos_dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
